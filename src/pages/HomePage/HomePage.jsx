cd4b8e08f10184897ce1fcad741615e6e5f0d979
Aslan <asln86@mail.ru>
2023-03-29 10:49:54 +0300

    start

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./HomePage.module.scss";
+
+
+export const HomePage = () => {
+  
+  return (
+    <div className={styles.home_page}>
+      
+    </div>
+  );
+};
