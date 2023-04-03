289c1d28e3d557b5fa20e2a524045892de0b52c0
islam <dead@MacBook-Air-islam-2.local>
2023-04-03 12:57:01 +0300

    сделал header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <header className={styles.header}>
+      <div className={styles.header2}>
+        <div className={styles.blok}>
+          <img
+            className={styles.img_logo}
+            src="apteka12.png"
+            alt="изображение"
+          />
+        </div>
 
-export default Header
+        <div className={styles.blok}>
+          <img
+            className={styles.admin_siz}
+            src="siz122.png"
+            alt="изображение"
+          />
+          <p>Профиль</p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
