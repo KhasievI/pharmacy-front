3d0c16976b0831f1b80a4455f365e47a168d04d4
islam <dead@MacBook-Air-islam-2.local>
2023-04-03 13:11:31 +0300

    сделал header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <div className={styles.header2}>
         <div className={styles.blok}>
           <img
-          
+
             className={styles.img_logo}
             src="apteka12.png"
             alt="изображение"
@@ -24,6 +24,7 @@ const Header = () => {
         </div>
       </div>
     </header>
+    
   );
 };
 
