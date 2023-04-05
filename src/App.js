05957611fced6e9b58b02d9e3a1ca0fa0aa5a579
islam <dead@MacBook-Air-islam-2.local>
2023-04-05 14:20:36 +0300

    создал бота

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import Header from "./components/Header/Header";
+
+import Menu from "./components/Menu/Menu";
+import { HomePage } from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
-        <Header/>
+        <Menu/>
+       <HomePage/>
     </div>
   );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import Chatbot from "../../Bot/Chatbot";
 import styles from "./HomePage.module.scss";
 
 
 export const HomePage = () => {
-  
+  const [active, setActive] = useState(false)
   return (
     <div className={styles.home_page}>
+       <Chatbot/>
       
     </div>
   );
